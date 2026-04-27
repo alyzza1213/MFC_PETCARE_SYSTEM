8804a8ca54415bfafed3ba7add1729ba0b95fd76
Alyzza Longboy <alyzzalongboy88@gmail.com>
2026-04-27 17:50:02 +0800

    update 27/04/2026

--- MFC_PETCARE_SYSTEM/settings.py
+++ MFC_PETCARE_SYSTEM/settings.py
@@ -133,6 +133,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "main", "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 
