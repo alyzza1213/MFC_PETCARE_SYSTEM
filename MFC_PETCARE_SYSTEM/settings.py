560a58e0208e2bff68405705a86efb0d2242e3f4
Alyzza Longboy <alyzzalongboy88@gmail.com>
2026-05-02 14:43:02 +0800

    update02/05/2026

--- MFC_PETCARE_SYSTEM/settings.py
+++ MFC_PETCARE_SYSTEM/settings.py
@@ -87,11 +87,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'MFC_PETCARE_SYSTEM.wsgi.application'
 
-cloudinary.config(
-    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    api_key=os.environ.get('CLOUDINARY_API_KEY'),
-    api_secret=os.environ.get('CLOUDINARY_API_SECRET')
-)
+
 
 
 # Database
