006ee8168fc93e4b48691456338386bb1ae0c36e
Bludvikk <cleverdmasoy@gmail.com>
2026-05-13 03:42:18 -0400

    Wire registration email and SMTP config

--- MFC_PETCARE_SYSTEM/settings.py
+++ MFC_PETCARE_SYSTEM/settings.py
@@ -225,12 +225,13 @@ else:
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = "smtp-relay.brevo.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp-relay.brevo.com")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "true").lower() in ("1", "true", "yes", "on")
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "false").lower() in ("1", "true", "yes", "on")
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_TIMEOUT = int(os.getenv("EMAIL_TIMEOUT", "10"))
 
-DEFAULT_FROM_EMAIL ="MFC Pet Care <noreply@mfcpetcare.xyz>"
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "MFC Pet Care <noreply@mfcpetcare.xyz>")
